1cc8350a9dbd25cfdeefdbcbd7243ca87c89cb31
wolpl <wolpl-dev@outlook.com>
2018-11-04 14:55:35 +0100

    adapt converter to ktor version 1.0.0-beta-3 and kotlin 1.3.0

--- src/main/kotlin/toliner/kotatu/KlaxonConverter.kt
+++ src/main/kotlin/toliner/kotatu/KlaxonConverter.kt
@@ -3,28 +3,30 @@ package toliner.kotatu
 import com.beust.klaxon.Klaxon
 import io.ktor.application.ApplicationCall
 import io.ktor.application.call
-import io.ktor.content.IncomingContent
 import io.ktor.content.TextContent
-import io.ktor.content.readText
 import io.ktor.features.ContentConverter
 import io.ktor.features.ContentNegotiation
 import io.ktor.features.suitableCharset
 import io.ktor.http.ContentType
 import io.ktor.http.withCharset
-import io.ktor.pipeline.PipelineContext
 import io.ktor.request.ApplicationReceiveRequest
+import io.ktor.util.pipeline.PipelineContext
+import kotlinx.coroutines.io.ByteReadChannel
+import kotlinx.coroutines.io.readRemaining
 
 class KlaxonConverter(private val klaxon: Klaxon = Klaxon()) : ContentConverter {
 
+    @io.ktor.util.KtorExperimentalAPI
     override suspend fun convertForSend(context: PipelineContext<Any, ApplicationCall>, contentType: ContentType, value: Any): Any? {
         return TextContent(klaxon.toJsonString(value), contentType.withCharset(context.call.suitableCharset()))
     }
 
     override suspend fun convertForReceive(context: PipelineContext<ApplicationReceiveRequest, ApplicationCall>): Any? {
         val request = context.subject
-        return klaxon.fromJsonObject(klaxon.parseJsonObject((request.value as? IncomingContent ?: return null).readText().reader()), request.type.javaObjectType, request.type)
+        val channel = request.value as? ByteReadChannel ?: return null
+        val reader = channel.readRemaining().readText().reader()
+        return klaxon.parse(reader)
     }
-
 }
 
 fun ContentNegotiation.Configuration.klaxon(block: Klaxon.() -> Unit) {
